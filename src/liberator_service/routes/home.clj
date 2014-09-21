415b24ad13705d94fc9c8eb63419bd8215f9f25a
Chris Howe-Jones <chris.howejones@gmail.com>
2014-09-21 20:29:27 +0100

    Initial commit.

---
+++ src/liberator_service/routes/home.clj
@@ -0,0 +1,18 @@
+(ns liberator-service.routes.home
+  (:require [compojure.core :refer :all]
+            [liberator.core :refer [defresource resource request-method-in]]))
+
+(defresource home
+  :service-available? true
+  :method-allowed? (request-method-in :get)
+  :handle-method-not-allowed
+  (fn [context]
+    (str (get-in context [:request :request-method])
+         " is not allowed"))
+  :handle-ok "Hello World!"
+  :etag "fixed-etag"
+  :available-media-types ["type/plain"])
+
+(defroutes home-routes
+  (ANY "/" request home))
+
